96c0c5443df6f205c8cfc86a367db5a39e18437c
Chris Alexander <chris.alexander@mclaren.com>
2020-02-14 17:57:33 +0000

    Demo 9

---
+++ demo-9-hyperloglog/Program.cs
@@ -0,0 +1,52 @@
+﻿using StackExchange.Redis;
+using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LoadShakespeare
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var multiplexer = await ConnectionMultiplexer.ConnectAsync("127.0.0.1:6379");
+
+            var f = File.OpenText(@"shakespeare.txt");
+
+            var lineNumber = 0;
+            string line;
+            while ((line = f.ReadLine()) != null)
+            {
+                lineNumber++;
+
+                // Skip headers and footers
+                if (lineNumber < 245 || lineNumber > 124438)
+                {
+                    continue;
+                }
+
+                var words = line.Split(' ').Where(w => w.Length > 0).ToList();
+
+                // Ignore short sentences
+                if (words.Count <= 3)
+                {
+                    continue;
+                }
+
+                foreach (var word in words)
+                {
+                    var insertWord = Regex.Replace(word, @"\p{P}", "").ToLower();
+
+                    multiplexer.GetDatabase().HyperLogLogAdd("shakespeare", insertWord);
+                }
+
+                if (lineNumber % 10000 == 0)
+                {
+                    Console.WriteLine($"Line {lineNumber} done");
+                }
+            }
+        }
+    }
+}
---
+++ demo-9-hyperloglog/README.md
@@ -0,0 +1,13 @@
+# Demo 9. HyperLogLog
+
+We use the included project to parse the text file, this is Data Science (TM) after all so the data needs cleaning.
+
+Once it is parsed we insert the unique words into a HyperLogLog, in order to count the number of unique words.
+
+Count at any stage with:
+
+    pfcount shakespeare
+
+Blank database:
+
+    flushdb
